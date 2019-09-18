ccaf719f4fbb16b8510b1447c16f9de74d3c1a33
rdelburgo <riccardo.del.burgo@cern.ch>
2019-09-18 16:32:57 +0200

    Create configuration_0.py

---
+++ analysis/test/configuration/configurations/configuration_0.py
@@ -0,0 +1,9 @@
+# configuration n.0
+decay_mode = [99,0,1,10]
+eta_range  = [0, 2.4]
+pt_range  = [5, 20]
+dt_cut_for_iso = 25*3*pow(2,0.5)
+max_dz = [0.2]
+dz_cut_for_iso = [0.2]
+status_final = 2 
+#############################
